77d9e51e0f0da9001da0134890a6dcaa6d4ec4a8
Andrew Hearts <andreyko.era@gmail.com>
2025-10-22 17:47:13 +0300

    Adding database samples fixed

--- Kursova/seed.py
+++ Kursova/seed.py
@@ -33,19 +33,87 @@ def seed_database():
 
     today = datetime.date.today()
 
-    for i in range(1, 11):
-        session.add(Car(id=i, brand=f"Brand{i}", model=f"Model{i}", year=random.randint(2000, 2025),
-                        vin=f"VIN{random.randint(0, 99999):05d}", technical_state="OK"))
-        session.add(Driver(id=i, name=f"Driver{i}", license_number=f"L{random.randint(0, 99999):05d}", experience=5 + i,
-                           medical_checks="Passed"))
+    session.add(Car(id=1, brand=f"Toyota", model=f"Corolla", year=random.randint(2000, 2025),
+                    vin=f"VIN{random.randint(0, 99999):05d}", technical_state="OK"))
+    session.add(Car(id=2, brand=f"Mercedes", model=f"Benz", year=random.randint(2000, 2025),
+                    vin=f"VIN{random.randint(0, 99999):05d}", technical_state="OK"))
+    session.add(Car(id=3, brand=f"Scoda", model=f"Octavia", year=random.randint(2000, 2025),
+                    vin=f"VIN{random.randint(0, 99999):05d}", technical_state="OK"))
+    session.add(Car(id=4, brand=f"Toyota", model=f"Land Cruiser", year=random.randint(2000, 2025),
+                    vin=f"VIN{random.randint(0, 99999):05d}", technical_state="OK"))
+    session.add(Car(id=5, brand=f"Opel", model=f"Astra", year=random.randint(2000, 2025),
+                    vin=f"VIN{random.randint(0, 99999):05d}", technical_state="OK"))
+    session.add(Car(id=6, brand=f"Lanos", model=f"Deo", year=random.randint(2000, 2025),
+                    vin=f"VIN{random.randint(0, 99999):05d}", technical_state="OK"))
+    session.add(Car(id=7, brand=f"Toyota", model=f"Corolla", year=random.randint(2000, 2025),
+                    vin=f"VIN{random.randint(0, 99999):05d}", technical_state="OK"))
+    session.add(Car(id=8, brand=f"Mitsubisi", model=f"Pagero", year=random.randint(2000, 2025),
+                    vin=f"VIN{random.randint(0, 99999):05d}", technical_state="OK"))
+    session.add(Car(id=9, brand=f"Hyundai", model=f"Sonata", year=random.randint(2000, 2025),
+                    vin=f"VIN{random.randint(0, 99999):05d}", technical_state="OK"))
+    session.add(Car(id=10, brand=f"Honda", model=f"Civic", year=random.randint(2000, 2025),
+                    vin=f"VIN{random.randint(0, 99999):05d}", technical_state="OK"))
+
+    session.add(Driver(id=1, name=f"Petro Shchur", license_number=f"L{random.randint(0, 99999):05d}", experience=6,
+                       medical_checks="Passed"))
+    session.add(Driver(id=2, name=f"Ivan Kolomyja", license_number=f"L{random.randint(0, 99999):05d}", experience=8,
+                       medical_checks="Passed"))
+    session.add(
+        Driver(id=3, name=f"Oleksandr Halytskyy", license_number=f"L{random.randint(0, 99999):05d}", experience=10,
+               medical_checks="Passed"))
+    session.add(Driver(id=4, name=f"Pavlo Kordola", license_number=f"L{random.randint(0, 99999):05d}", experience=5,
+                       medical_checks="Passed"))
+    session.add(Driver(id=5, name=f"Stepan Pidhayets", license_number=f"L{random.randint(0, 99999):05d}", experience=1,
+                       medical_checks="Passed"))
+    session.add(Driver(id=6, name=f"Sofiya Zayets", license_number=f"L{random.randint(0, 99999):05d}", experience=4,
+                       medical_checks="Passed"))
+    session.add(Driver(id=7, name=f"Daryna Mandaryna", license_number=f"L{random.randint(0, 99999):05d}", experience=15,
+                       medical_checks="Passed"))
+    session.add(Driver(id=8, name=f"Andrii Perun", license_number=f"L{random.randint(0, 99999):05d}", experience=22,
+                       medical_checks="Passed"))
+    session.add(Driver(id=9, name=f"Oleg Kos", license_number=f"L{random.randint(0, 99999):05d}", experience=16,
+                       medical_checks="Passed"))
+    session.add(Driver(id=10, name=f"Eduard Ivanenko", license_number=f"L{random.randint(0, 99999):05d}", experience=3,
+                       medical_checks="Passed"))
+
+    session.add(
+        Trip(id=1, date=today, route=f"Kyiv-Lviv", distance=556, car_id=random.randint(1, 10),
+             driver_id=random.randint(1, 10)))
+    session.add(
+        Trip(id=2, date=today, route=f"Rivne-Lviv", distance=315, car_id=random.randint(1, 10),
+             driver_id=random.randint(1, 10)))
+    session.add(
+        Trip(id=3, date=today, route=f"Kyiv-Kramatorsk", distance=225, car_id=random.randint(1, 10),
+             driver_id=random.randint(1, 10)))
+    session.add(
+        Trip(id=4, date=today, route=f"Ternopil-Ratne", distance=480, car_id=random.randint(1, 10),
+             driver_id=random.randint(1, 10)))
+    session.add(
+        Trip(id=5, date=today, route=f"Poltava-Lviv", distance=1112, car_id=random.randint(1, 10),
+             driver_id=random.randint(1, 10)))
+    session.add(
+        Trip(id=6, date=today, route=f"Kyiv-Dnipro", distance=356, car_id=random.randint(1, 10),
+             driver_id=random.randint(1, 10)))
+    session.add(
+        Trip(id=7, date=today, route=f"Uzhorod-Ternopil", distance=682, car_id=random.randint(1, 10),
+             driver_id=random.randint(1, 10)))
+    session.add(
+        Trip(id=8, date=today, route=f"Briukhovychi-Lviv", distance=5, car_id=random.randint(1, 10),
+             driver_id=random.randint(1, 10)))
+    session.add(
+        Trip(id=9, date=today, route=f"Ratne-Lutsk", distance=82, car_id=random.randint(1, 10),
+             driver_id=random.randint(1, 10)))
+    session.add(
+        Trip(id=10, date=today, route=f"Khmelnytskyy-Cherkasy", distance=432, car_id=random.randint(1, 10),
+             driver_id=random.randint(1, 10)))
+
+    for i in range(1,11):
         session.add(
-            Trip(id=i, date=today, route=f"Route {i}", distance=random.randint(1, 1000), car_id=random.randint(1, 11),
-                 driver_id=random.randint(1, 11)))
-        session.add(FuelCost(id=i, date=today, type="Petrol", cost=random.randint(50, 1000), car_id=random.randint(1, 11)))
-        session.add(Maintenance(id=i, date=today, type="TO", cost=random.randint(100, 10000), done=False, car_id=random.randint(1, 11)))
+            FuelCost(id=i, date=today, type="Petrol", cost=random.randint(50, 1000), car_id=random.randint(1, 11)))
+        session.add(Maintenance(id=i, date=today, type="TO", cost=random.randint(100, 10000), done=False,
+                                car_id=random.randint(1, 11)))
         expiry = today + datetime.timedelta(days=30 * i)
         session.add(Insurance(id=i, policy_number=f"Policy{i}", expiry_date=expiry, cost=50, car_id=i))
-
 
     session.commit()
     session.close()
